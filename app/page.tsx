71bfb1a87f2fb5ef5f5cac33129fad3de6522122
DAndre06d <dumalayda06@gmail.com>
2024-11-03 21:47:28 +0800

    Tailwind setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="text-3xl text-violet-700 font-bold">Welcome to NextJs.</div>
-  );
+    return (
+        <h1 className="h1-bold background-lightandDark">
+            Welcome to world of next js
+        </h1>
+    );
 }
