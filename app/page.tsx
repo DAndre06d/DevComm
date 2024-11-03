a6c0f924b347d090fd802d2662054c72d886152d
DAndre06d <dumalayda06@gmail.com>
2024-11-03 22:16:28 +0800

    Added Assets and Metadata.

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <h1 className="h1-bold background-lightandDark">
-            Welcome to world of next js
-        </h1>
+        <>
+            <h1 className="h1-bold background-lightandDark">
+                Welcome to world of next js
+            </h1>
+            <h1 className="h1-bold background-lightandDark font-spaceGrotesk">
+                Welcome to world of next js
+            </h1>
+        </>
     );
 }
