8731a9f4d48e0a2c2326c24d6deec276e2768617
DAndre06d <dumalayda06@gmail.com>
2024-11-05 01:05:18 +0800

    Added Light and Dark Theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
 import Image from "next/image";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Home() {
     return (
@@ -9,6 +17,17 @@ export default function Home() {
             <h1 className="h1-bold background-lightandDark font-spaceGrotesk">
                 Welcome to world of next js
             </h1>
+            <DropdownMenu>
+                <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                    <DropdownMenuItem>Billing</DropdownMenuItem>
+                    <DropdownMenuItem>Team</DropdownMenuItem>
+                    <DropdownMenuItem>Subscription</DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </>
     );
 }
