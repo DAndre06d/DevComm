0f6f986b57b8ce6fa0bbf93d60584fd84178d85c
DAndre06d <dumalayda06@gmail.com>
2024-11-12 01:25:31 +0800

    Added left SIdebar and right sidebar

--- constants/routes.ts
+++ constants/routes.ts
@@ -2,11 +2,8 @@ const ROUTES = {
     HOME: "/",
     SIGN_IN: "/sign-in",
     SIGN_UP: "/sign-up",
-    COMUNITY: "/community",
-    COLLECTION: "/collection",
-    JOBS: "/find-jobs",
-    TAGS: "/tags",
-    PROFILE: "/profile",
+    PROFILE: (id: string) => `/profile/${id}`,
+    TAGS: (id: string) => `/profile/${id}`,
     ASK_QUESTION: "/ask-a-question",
 };
 
