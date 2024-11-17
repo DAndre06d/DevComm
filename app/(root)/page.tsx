85495b17cd0d67901568a5758193add1a598ad17
DAndre06d <dumalayda06@gmail.com>
2024-11-17 15:17:53 +0800

    Added Question card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import { auth, signOut } from "@/auth";
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
@@ -13,11 +14,15 @@ const questions = [
             { _id: "1", name: "React" },
             { _id: "2", name: "Javascript" },
         ],
-        author: { _id: "2", name: "John Doe" },
-        upVotes: 10,
+        author: {
+            _id: "2",
+            name: "John Doe",
+            image: "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+        },
+        upvotes: 10,
         answers: 5,
         views: 100,
-        createdAt: new Date(),
+        createdAt: new Date("2021-09-21"),
     },
     {
         _id: "2",
@@ -27,8 +32,12 @@ const questions = [
             { _id: "1", name: "React" },
             { _id: "2", name: "Javascript" },
         ],
-        author: { _id: "1", name: "John Doe" },
-        upVotes: 10,
+        author: {
+            _id: "1",
+            name: "John Doe",
+            image: "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+        },
+        upvotes: 10,
         answers: 5,
         views: 100,
         createdAt: new Date(),
@@ -76,7 +85,7 @@ const Home = async ({ searchParams }: SearchParamsProps) => {
             <HomeFilter />
             <div className="mt-10 flex w-full flex-col gap-6 ">
                 {filteredQuestions.map((question) => (
-                    <h1 key={question._id}>{question.title}</h1>
+                    <QuestionCard key={question._id} question={question} />
                 ))}
             </div>
         </>
--- constants/routes.ts
+++ constants/routes.ts
@@ -4,6 +4,7 @@ const ROUTES = {
     SIGN_UP: "/sign-up",
     PROFILE: (id: string) => `/profile/${id}`,
     TAGS: (id: string) => `/profile/${id}`,
+    QUESTIONS: (id: string) => `/questions/${id}`,
     ASK_QUESTION: "/ask-a-question",
 };
 
