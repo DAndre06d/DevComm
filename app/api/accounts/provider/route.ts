6ddd95d83b9585ef785a33c196cb0a92b02f91c3
DAndre06d <dumalayda06@gmail.com>
2024-11-30 14:47:23 +0800

    Added get account by provider

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,28 @@
+import Account from "@/database/accounts.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import { AccountSchema } from "@/lib/validation";
+import { APIErrorResponse } from "@/types/global";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    const { providerAccountId } = await request.json();
+    try {
+        const validatedData = AccountSchema.partial().safeParse({
+            providerAccountId,
+        });
+
+        if (!validatedData.success)
+            throw new ValidationError(
+                validatedData.error.flatten().fieldErrors
+            );
+        const account = await Account.findOne({ providerAccountId });
+        if (!account) throw new NotFoundError("Account");
+        return NextResponse.json(
+            { success: true, data: account },
+            { status: 200 }
+        );
+    } catch (e) {
+        return handleError(e, "api") as APIErrorResponse;
+    }
+}
